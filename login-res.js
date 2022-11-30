45533484977c2cf0af107ccf8a6af286e740af23
unknown <nikhilpillare98@gmail.com>
2022-11-30 19:20:21 +0530

    updated_check_it_once

--- login-res.js
+++ login-res.js
@@ -1,2 +1,11 @@
 let reg_email = "amrit@gmail.com"
 let reg_pass = " 1234@"
+
+
+let loginEmail= "amrit@gmail.com"
+let loginPassword= "1234@"
+
+
+if(reg_email==loginEmail && reg_pass==loginPassword){
+           console.log("welcome")
+   }else{console.log("wrong creditials")};
